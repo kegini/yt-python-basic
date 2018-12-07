25427e26d3989dcd31c575880e8506c778a41134
kegini <45682208+kegini@users.noreply.github.com>
2018-12-07 14:39:10 +0700

    Create 1_variable.py

---
+++ 1_variable.py
@@ -0,0 +1,14 @@
+var1 = 0                 # Bilangan Bulat (integer)
+var2 = 0.1               # Bilangan Asli (Real)
+var3 = '0'               # Character
+var4 = "0"               # String
+var5 = [0,1,2,3,4,5]     # Array 1 dimensi
+var6 = [[0,1,2],[3,4,5]] # Array 2 dimensi 2x3
+
+# Menampilan Variable
+print("var1 adalah " + var1)
+print("var2 adalah " + var2)
+print("var3 adalah " + var3)
+print("var4 adalah " + var4)
+print("var5 adalah " + var5)
+print("var6 adalah " + var6)
